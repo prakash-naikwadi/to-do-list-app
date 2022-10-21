e616133d202a1945e65cc1896e1f5ff4ab02c096
prakash naikwadi <naikwadiprakash1@gmail.com>
2022-10-21 18:16:13 +0530

    delete functionality aa

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,8 +2,15 @@ function Button({ name, type, classname }) {
   // console.log(type);
 
   const deleteListItem = (e) => {
-    // console.log(e);
+    const newObj = JSON.parse(localStorage.getItem("myData"));
+    const key = e.target.parentElement.getAttribute("data-key");
+    const i = newObj.findIndex((item) => item.key === parseInt(key));
+
     e.target.parentElement.remove();
+
+    newObj.splice(i, 1);
+
+    localStorage.setItem("myData", JSON.stringify(newObj));
   };
 
   return (
